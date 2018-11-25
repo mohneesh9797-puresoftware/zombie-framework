9444a526edcd4199ac27873a9ad3863b345d591d
minexew <minexew@gmail.com>
2018-11-25 14:30:12 +0100

    Add ContainerDeferredShading

---
+++ examples/ContainerDeferredShading/src/components.hpp
@@ -0,0 +1,29 @@
+#ifndef CONTAINERDEFERREDSHADING_POINTLIGHT_HPP
+#define CONTAINERDEFERREDSHADING_POINTLIGHT_HPP
+
+#include <framework/datamodel.hpp>
+
+#include <string>
+
+namespace example
+{
+    using namespace zfw;
+
+    struct PointLight {
+        bool castsShadows = true;
+        Float3 pos;
+        Float3 ambient;
+        Float3 diffuse;
+        float range;
+    };
+
+    struct SkyBox {
+        std::string texturePath;
+    };
+
+    struct WorldGeometry {
+        std::string recipe;
+    };
+}
+
+#endif //CONTAINERDEFERREDSHADING_POINTLIGHT_HPP
