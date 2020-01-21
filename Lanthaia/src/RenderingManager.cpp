c6044db3ad7b3d3b992ad17ee43977c5d52395cd
minexew <minexew@gmail.com>
2020-01-21 22:19:21 +0100

    Lanthaia: draw something in 3D

--- Lanthaia/src/RenderingManager.cpp
+++ Lanthaia/src/RenderingManager.cpp
@@ -4,6 +4,8 @@
 #include <framework/engine.hpp>
 #include <framework/errorcheck.hpp>
 #include <framework/utility/essentials.hpp>
+
+#include <RenderingKit/utility/Camera.hpp>
 #include <RenderingKit/utility/RKVideoHandler.hpp>
 
 using namespace RenderingKit;
@@ -23,6 +25,8 @@ bool RenderingManager::Startup(zfw::IEngine* sys, zfw::MessageQueue* eventQueue)
 
     auto wm = rk->GetWindowManager();
 
+    // TODO API: refactor stateful LoadDefaultSettings
+    // TODO API: consider enabling anti-alias by default
     if (!wm->LoadDefaultSettings(nullptr) || !wm->ResetVideoOutput()) {
         return false;
     }
@@ -32,11 +36,24 @@ bool RenderingManager::Startup(zfw::IEngine* sys, zfw::MessageQueue* eventQueue)
 
     sys->SetVideoHandler(make_unique<RKVideoHandler>(rm, wm, eventQueue));
 
+    zombie_ErrorCheck(bp2d.Init(rm));
+    zombie_ErrorCheck(bp3d.Init(rm));
+
     return true;
 }
 
 void RenderingManager::DrawWorld() {
     rm->Clear(zfw::COLOUR_WHITE);
+
+    // TODO API: it's too easy to forget something
+    Camera cam(CoordinateSystem::leftHanded);
+    cam.SetClippingDist(1.0f, 1000.0f);
+    cam.SetVFov(3.14f * 0.25f);
+    cam.SetPerspective();
+    cam.SetViewWithCenterDistanceYawPitch({}, 20.0f, 0.0f, 3.14f*0.25f);
+    rm->SetCamera(&cam);
+
+    bp3d.DrawGridAround({}, {1.0f, 1.0f, 0.0f}, {20, 20}, zfw::RGBA_BLACK);
 }
 
 }
