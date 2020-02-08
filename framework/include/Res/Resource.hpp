8495eeb4577723b96bbaf511e84a66c46a290525
minexew <minexew@gmail.com>
2020-02-08 22:59:56 +0100

    framework: borrow Obs::Res::IResource, Obs::Res::IResourceManager

---
+++ framework/include/Res/Resource.hpp
@@ -0,0 +1,13 @@
+#ifndef OBS_RES_RESOURCE_HPP
+#define OBS_RES_RESOURCE_HPP
+
+#include <framework/resource.hpp>
+
+namespace Obs::Res {
+
+using IResource = zfw::IResource2;
+using ResourceParamSet = zfw::ResourceParamSet;
+
+}
+
+#endif
---
+++ framework/include/Res/ResourceManager.hpp
@@ -0,0 +1,12 @@
+#ifndef OBS_RES_RESOURCEMANAGER_HPP
+#define OBS_RES_RESOURCEMANAGER_HPP
+
+#include <framework/resourcemanager2.hpp>
+
+namespace Obs::Res {
+
+using IResourceManager = zfw::IResourceManager2;
+
+}
+
+#endif
